81cf185db65c312a10f58a80b6cc402cba22bc44
Luke Jagger <luke.jagger@alfresco.com>
2013-06-27 09:26:56 +0100

    Working folder and document handling

---
+++ ComAlfrescoAppceleratorIosSdkDocumentProxy.m
@@ -0,0 +1,13 @@
+//
+//  ComAlfrescoAppceleratorIosSdkFolder.m
+//  iOSSDKModule
+//
+//  Created by Luke Jagger on 17/06/2013.
+//
+//
+
+#import "ComAlfrescoAppceleratorIosSdkFolder.h"
+
+@implementation ComAlfrescoAppceleratorIosSdkFolder
+
+@end
---
+++ ComAlfrescoAppceleratorIosSdkFolderProxy.h
@@ -0,0 +1,13 @@
+//
+//  ComAlfrescoAppceleratorIosSdkFolder.h
+//  iOSSDKModule
+//
+//  Created by Luke Jagger on 17/06/2013.
+//
+//
+
+#import "TiProxy.h"
+
+@interface ComAlfrescoAppceleratorIosSdkFolder : TiProxy
+
+@end
