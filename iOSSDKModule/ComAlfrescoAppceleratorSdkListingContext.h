f42cb7bcf1675ca4697641fdb556e3793e3e5d9d
Luke Jagger <luke.jagger@alfresco.com>
2013-08-22 16:17:19 +0100

    Adding pagingResult / ListingContext support. Tidied Sites and Activities code with common functionality modularised. Property display support, with switch to turn on/off.

---
+++ iOSSDKModule/ComAlfrescoAppceleratorSdkListingContext.h
@@ -0,0 +1,13 @@
+//
+//  ComAlfrescoAppceleratorSdkListingContext.h
+//  iOSSDKModule
+//
+//  Created by Luke Jagger on 22/08/2013.
+//
+//
+
+#import "TiProxy.h"
+
+@interface ComAlfrescoAppceleratorSdkListingContext : TiProxy
+
+@end
---
+++ iOSSDKModule/ComAlfrescoAppceleratorSdkListingContext.m
@@ -0,0 +1,13 @@
+//
+//  ComAlfrescoAppceleratorSdkListingContext.m
+//  iOSSDKModule
+//
+//  Created by Luke Jagger on 22/08/2013.
+//
+//
+
+#import "ComAlfrescoAppceleratorSdkListingContext.h"
+
+@implementation ComAlfrescoAppceleratorSdkListingContext
+
+@end
