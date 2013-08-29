a8fa478be6c138d0fb434c823030a6305d0cbf70
Luke Jagger <luke.jagger@alfresco.com>
2013-08-29 12:41:54 +0100

    Added support objects for keyword search and listing context.

---
+++ iOSSDKModule/Classes/ComAlfrescoAppceleratorSdkKeywordSearchOptionsProxy.h
@@ -0,0 +1,13 @@
+//
+//  ComAlfrescoAppceleratorSdkKeywordSearchOptionsProxy.h
+//  iOSSDKModule
+//
+//  Created by Luke Jagger on 29/08/2013.
+//
+//
+
+#import "TiProxy.h"
+
+@interface ComAlfrescoAppceleratorSdkKeywordSearchOptionsProxy : TiProxy
+
+@end
---
+++ iOSSDKModule/Classes/ComAlfrescoAppceleratorSdkKeywordSearchOptionsProxy.m
@@ -0,0 +1,13 @@
+//
+//  ComAlfrescoAppceleratorSdkKeywordSearchOptionsProxy.m
+//  iOSSDKModule
+//
+//  Created by Luke Jagger on 29/08/2013.
+//
+//
+
+#import "ComAlfrescoAppceleratorSdkKeywordSearchOptionsProxy.h"
+
+@implementation ComAlfrescoAppceleratorSdkKeywordSearchOptionsProxy
+
+@end
