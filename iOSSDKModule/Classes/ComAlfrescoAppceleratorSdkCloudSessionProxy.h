9142d83a4da4724b155ddb9b2452eefa3b3b737f
Luke Jagger <luke.jagger@alfresco.com>
2014-02-03 13:34:31 +0000

    Adding Cloud session classes.

---
+++ iOSSDKModule/Classes/ComAlfrescoAppceleratorSdkCloudSessionProxy.h
@@ -0,0 +1,71 @@
+/*
+ ******************************************************************************
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ *
+ * This file is part of the Alfresco Mobile SDK.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *****************************************************************************
+ */
+
+//
+//  AlfrescoRepositorySessionProxy.h
+//  iOSSDKModule
+//
+//  Created by Luke Jagger on 20/05/2013.
+//
+//
+
+#import "TiProxy.h"
+#import "ComAlfrescoAppceleratorSdkRequestProxy.h"
+#import "ComAlfrescoAppceleratorSdkSessionProxy.h"
+
+/**
+ 
+#Javascript object:#
+<code>RepositorySession</code>
+
+#Required properties:#
+* String serverUrl
+* String serverUsername
+* String serverPassword
+ 
+#Javascript events:#
+* **'error' - ** Sent upon error condition from any API.  ***Properties:*** *string errorstring, int errorcode*
+* **'success' - ** Sent upon successful connection.  ***Properties:*** *string serverName*
+ 
+#Javascript example:#
+ 
+    var SDKModule = require('com.alfresco.appcelerator.module.sdk');
+ 
+    var properties  = {serverUrl: "http://localhost:8080/alfresco",  serverUsername: "admin",  serverPassword: "pwd"};
+ 
+    var repoSession = SDKModule.createRepositorySession(properties);
+ 
+    repoSession.addEventListener('error', function(e) { alert("Cannot connect to server (" + e.errorcode + "): " + e.errorstring); } );
+ 
+    repoSession.addEventListener('success',function(e) { Ti.API.info("Connected to server: " + e.servername); } );
+ 
+    repoSession.connect();
+ 
+*/
+
+@interface ComAlfrescoAppceleratorSdkRepositorySessionProxy : ComAlfrescoAppceleratorSdkSessionProxy
+
+/** Connect to the repository using the properties set at creation
+@since v1.0
+*/
+-(void)connect:(id)noargs;
+
+@end
+
---
+++ iOSSDKModule/Classes/ComAlfrescoAppceleratorSdkCloudSessionProxy.m
@@ -0,0 +1,73 @@
+/*
+ ******************************************************************************
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ *
+ * This file is part of the Alfresco Mobile SDK.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *****************************************************************************
+ */
+
+//
+//  AlfrescoRepositorySessionProxy.m
+//  iOSSDKModule
+//
+//  Created by Luke Jagger on 20/05/2013.
+//
+//
+
+#import "ComAlfrescoAppceleratorSdkRepositorySessionProxy.h"
+#import "AlfrescoRepositorySession.h"
+#import "TiUtils.h"
+#include "SDKUtil.h"
+
+
+@implementation ComAlfrescoAppceleratorSdkRepositorySessionProxy
+
+ 
+-(void)connect:(id)noargs
+{
+    ENSURE_UI_THREAD_0_ARGS
+    
+    NSURL *url = [NSURL URLWithString:[self valueForKey:@"serverUrl"]];
+    NSString *user = [self valueForKey:@"serverUsername"];
+    NSString *pwd = [self valueForKey:@"serverPassword"];
+    
+    if (url == nil  ||  user == nil  ||  pwd == nil)
+    {
+        [SDKUtil createParamErrorEvent:self];
+        return;
+    } 
+    
+    ComAlfrescoAppceleratorSdkRepositorySessionProxy *weakSelf = self;
+  
+    [AlfrescoRepositorySession connectWithUrl:url username:user password:pwd
+                                                        completionBlock:^(id<AlfrescoSession> session, NSError *error)
+                                                        {
+                                                            if (nil == session)
+                                                            {
+                                                                self.error = error;                                                                
+                                                                [SDKUtil createErrorEvent:error proxyObject:weakSelf];
+                                                            }
+                                                            else
+                                                            {
+                                                                weakSelf.session = session;
+                                                                weakSelf.info = weakSelf.session.repositoryInfo;
+                                                                
+                                                                NSDictionary *event = [NSDictionary dictionaryWithObjectsAndKeys:weakSelf.info.name, @"servername", nil];
+                                                                [weakSelf fireEvent:@"success" withObject:event];
+                                                            }
+                                                        }];
+}
+
+@end
